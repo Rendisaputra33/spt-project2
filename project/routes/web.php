249140edb8276bf3ba4f8218542753892a7a4a21
dnothingnes <rendisaputraofficial@gmail.com>
2021-07-17 20:57:17 +0700

    update

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\authcontroller;
+use App\Http\Controllers\petanicontroller;
+use App\Http\Controllers\usercontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +16,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/auth', function () {
-    Route::get('login', [authcontroller::class, 'index']);
+// routing authenticate
+Route::prefix('auth')->group(function () {
+    Route::post('login', [authcontroller::class, 'index']);
+    Route::get('logout', [authcontroller::class, 'logout']);
+    Route::post('register', [authcontroller::class, 'register']);
+});
+// routing master user
+Route::prefix('user')->group(function () {
+    // default routing
+    Route::get('/', [usercontroller::class, 'index']);
+    Route::post('/', [usercontroller::class, 'add']);
+    Route::put('/{id}', [usercontroller::class, 'update']);
+    Route::get('/{id}', [usercontroller::class, 'delete']);
+    // json handler
+    Route::prefix('json')->group(function () {
+        Route::get('/{id}', [usercontroller::class, 'getUpdate']);
+    });
+});
+// routing master petani
+Route::prefix('petani')->group(function () {
+    // default routing
+    Route::get('/', [petanicontroller::class, 'index']);
+    Route::post('/', [petanicontroller::class, 'add']);
+    Route::put('/{id}', [petanicontroller::class, 'update']);
+    Route::get('/{id}', [petanicontroller::class, 'delete']);
+    // json handler
+    Route::prefix('json')->group(function () {
+        Route::get('/{id}', [petanicontroller::class, 'getUpdate']);
+    });
 });
 
 Route::get('/', function () {
