645468df13b684f3506a6d76be5e7dee88babae4
dnothingnes <rendisaputraofficial@gmail.com>
2021-07-19 01:14:15 +0700

    update

--- project/resources/views/tampil-data-petani.blade.php
+++ project/resources/views/tampil-data-petani.blade.php
@@ -1,50 +1,51 @@
-
 @extends('template.template')
 @section('content')
     <div class="main-panel">
         <div class="content-wrapper">
-        <div class="col-lg-12 p-0 d-flex justify-content-between">
-        <h2>Data Petani</h2>
-        <button type="button" class="btn btn-success btn-icon" data-target="#modal-md-tambah" id='tbh' data-toggle="modal">
-            <i class="mdi mdi-plus"></i>
-          </button>
-        </div>
-          <div class="col-lg-12 grid-margin stretch-card p-0 mt-3">
-              <div class="card">
-                <div class="card-body">
-                  <table class="table table-hover">
-                    <thead>
-                      <tr>
-                        <th>No</th>
-                        <th>REG</th>
-                        <th>Nama Pemilik</th>
-                        <th>Tanggal Masuk</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                          <td>1</td>
-                          <td>dummy</td>
-                          <td>dummy</td>
-                          <td>dummy</td>
-                          <td>
-                              <button type="button" class="btn btn-sm btn-warning btn-icon-text">
-                                  <i class="mdi mdi-lead-pencil btn-icon-prepend"></i> Ubah </button>
-                              <button type="button" class="btn btn-sm btn-danger btn-icon-text">
-                                  <i class="mdi mdi-delete btn-icon-prepend"></i> Hapus </button>
-                          </td>
+            <div class="col-lg-12 p-0 d-flex justify-content-between">
+                <h2>Data Petani</h2>
+                <button type="button" class="btn btn-success btn-icon" data-target="#modal-md-tambah" id='tbh' data-toggle="modal">
+                    <i class="mdi mdi-plus"></i>
+                </button>
+            </div>
+            <div class="col-lg-12 grid-margin stretch-card p-0 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>No Induk</th>
+                                    <th>Nama Petani</th>
+                                    <th>Tanggal Masuk</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $no = 1; ?>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td>{{ $no++ }}</td>
+                                        <td>{{ $item->reg }}</td>
+                                        <td>{{ $item->nama_petani }}</td>
+                                        <td>{{ date('d/m/Y', strtotime($item->created_at)) }}</td>
+                                        <td>
+                                            <button type="button" class="btn btn-sm btn-warning btn-icon-text update" data-target="#modal-md-tambah" id='tbh' data-toggle="modal" data-id="{{ $item->id_petani }}">
+                                                <i class="mdi mdi-lead-pencil btn-icon-prepend"></i> Ubah </button>
+                                            <a class="btn btn-sm btn-danger btn-icon-text delete" href="{{ url('/petani') }}/{{ $item->id_user }}"> <i class="mdi mdi-delete btn-icon-prepend"></i> Hapus </a>
+                                        </td>
 
-                      </tr>
-                    </tbody>
-                  </table>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-              </div>
             </div>
         </div>
         <footer class="footer">
             <div class="container-fluid clearfix">
-              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
+                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
             </div>
         </footer>
     </div>
@@ -81,6 +82,6 @@
             <!-- /.modal-content -->
         </div>
         <!-- /.modal-dialog -->
-</div>
-</form>
+        </div>
+    </form>
 @endsection
