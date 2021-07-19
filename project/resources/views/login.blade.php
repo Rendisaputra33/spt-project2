6b13ff72f468f1bc87adafc6f64630efd6175064
TryLuthfi <ty01091990@gmail.com>
2021-07-19 12:06:55 +0700

    Test

--- project/resources/views/login.blade.php
+++ project/resources/views/login.blade.php
@@ -1,42 +1,43 @@
 @extends('template.loginlayout')
 @section('content')
-    <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth">
-                <div class="row flex-grow">
-                    <div class="col-lg-4 mx-auto err-msg">
-                        <div class="msg">
-                            @if (session('error') !== null)
-                                <x-alert type="danger" message="{{ session('error') }}" />
-                            @endif
+<div class="container-scroller">
+    <!-- addslashes -->
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+            <div class="row flex-grow">
+                <div class="col-lg-4 mx-auto err-msg">
+                    <div class="msg">
+                        @if (session('error') !== null)
+                        <x-alert type="danger" message="{{ session('error') }}" />
+                        @endif
+                    </div>
+                    <div class="auth-form-light text-left p-5">
+                        <div class="brand-logo">
+                            <h2 class="">Login</h2>
                         </div>
-                        <div class="auth-form-light text-left p-5">
-                            <div class="brand-logo">
-                                <h2 class="">Login</h2>
+                        <h4>Hello! let's get started</h4>
+                        <h6 class="font-weight-light">Sign in to continue.</h6>
+                        <form class="pt-3" action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" placeholder="Username" name="username">
                             </div>
-                            <h4>Hello! let's get started</h4>
-                            <h6 class="font-weight-light">Sign in to continue.</h6>
-                            <form class="pt-3" action="{{ route('login') }}" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-lg" placeholder="Username" name="username">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-lg" placeholder="Password" name="password">
-                                </div>
-                                <div class="mt-3">
-                                    <button type="submit" class="btn btn-block btn-success  btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
-                                </div>
-                            </form>
-                        </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" placeholder="Password" name="password">
+                            </div>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-block btn-success  btn-lg font-weight-medium auth-form-btn">SIGN IN</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-            <!-- content-wrapper ends -->
         </div>
-        <!-- page-body-wrapper ends -->
+        <!-- content-wrapper ends -->
     </div>
+    <!-- page-body-wrapper ends -->
+</div>
 @endsection
 @section('specific-js')
-    <script src="{{ asset('assets/js/function/Auth.js') }}"></script>
+<script src="{{ asset('assets/js/function/Auth.js') }}"></script>
 @endsection
