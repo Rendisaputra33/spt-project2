356ed8cf0cc2da4b1497e2b4d904ab0a7f54032a
dnothingnes <rendisaputraofficial@gmail.com>
2021-07-21 12:56:42 +0700

    update

--- project/resources/views/tampil-data-petani.blade.php
+++ project/resources/views/tampil-data-petani.blade.php
@@ -66,14 +66,14 @@
             </div>
         </div>
         <!-- <footer class="footer">
-                <div class="container-fluid clearfix">
-                    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
-                </div>
-            </footer> -->
+                    <div class="container-fluid clearfix">
+                        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
+                    </div>
+                </footer> -->
     </div>
     <!-- modal untuk tambah data -->
-    <form action="{{ asset('/petani') }}" method="post" id="form-">
+    <form action="{{ url('/petani') }}" method="post" id="form-">
         @csrf
         <div id="method"></div>
         <div class="modal fade" id="modal-md-tambah">
