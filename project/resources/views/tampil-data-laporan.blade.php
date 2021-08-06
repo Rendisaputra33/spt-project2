bc103d60e82de31d97807f3e4803563e8f319733
TryLuthfi <ty01091990@gmail.com>
2021-08-06 14:38:00 +0700

    FIlter Masa Giling

--- project/resources/views/tampil-data-laporan.blade.php
+++ project/resources/views/tampil-data-laporan.blade.php
@@ -1,93 +1,93 @@
 @extends('template.template')
 @section('content')
-    <div class="main-panel">
-        <div class="content-wrapper px-2 ">
-            <div class="col-lg-12 p-0 d-flex justify-content-between">
-                <h2>Data Laporan</h2>
-                <div class="right d-flex align-items-center">
-                    <button type="button" class="btn btn-gradient-success btn-icon-text d-flex" data-target="#modal-md-filter" id='tbh' data-toggle="modal">
-                        <i class="mdi mdi-filter-outline"></i>Filter
-                    </button>
-                    &nbsp;
-                    <a class="btn btn-gradient-success btn-icon-text d-flex" href="{{ url('/laporan/cetak/') . '/' }}{{ !isset($filter) ? 'month' : $filter['type'] . '=' . $filter['data'] }}">
-                        <i class="mdi mdi-printer"></i>Cetak
-                    </a>
-                </div>
+<div class="main-panel">
+    <div class="content-wrapper px-2 ">
+        <div class="col-lg-12 p-0 d-flex justify-content-between">
+            <h2>Data Laporan</h2>
+            <div class="right d-flex align-items-center">
+                <button type="button" class="btn btn-gradient-success btn-icon-text d-flex" data-target="#modal-md-filter" id='tbh' data-toggle="modal">
+                    <i class="mdi mdi-filter-outline"></i>Filter
+                </button>
+                &nbsp;
+                <a class="btn btn-gradient-success btn-icon-text d-flex" href="{{ url('/laporan/cetak/') . '/' }}{{ !isset($filter) ? 'month' : $filter['type'] . '=' . $filter['data'] }}">
+                    <i class="mdi mdi-printer"></i>Cetak
+                </a>
             </div>
-            <div class="col-lg-12 grid-margin stretch-card p-0 mt-3">
-                <div class="card">
-                    <div class="card-body">
-                        <table class="table table-sm table-hover">
-                            <thead>
-                                <tr>
-                                    <th>NO</th>
-                                    <th>MG</th>
-                                    <th>PERIODE</th>
-                                    <th>TANGGAL</th>
-                                    <th>PABRIK</th>
-                                    <th>REG</th>
-                                    <th>PETANI</th>
-                                    <th>NO SPTA</th>
-                                    <th>NO TRUK</th>
-                                    <th>BOBOT</th>
-                                    <th>VAR</th>
-                                    <th>TYPE</th>
-                                    <th>KET</th>
-                                    <th>BELI</th>
-                                    <th>HPP</th>
-                                    <th>SISA</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $no = 1; ?>
-                                <?php $sisa = 0; ?>
-                                <?php $bobot = 0; ?>
-                                @foreach ($data as $item)
-                                    <?php $sisa += $item->sisa; ?>
-                                    <?php $bobot += $item->bobot; ?>
-                                    <tr style="height: 52px;">
-                                        <td>{{ $no++ }}</td>
-                                        <td>{{ $item->masa_giling }}</td>
-                                        <td>{{ $item->periode }}</td>
-                                        <td>{{ date('d/M/Y', strtotime($item->created_at)) }}</td>
-                                        <td>{{ $item->pabrik }}</td>
-                                        <td>{{ $item->reg }}</td>
-                                        <td>{{ $item->petani }}</td>
-                                        <td>{{ $item->nospta }}</td>
-                                        <td>{{ $item->nopol }}</td>
-                                        <td>{{ $item->bobot }}</td>
-                                        <td>{{ $item->variasi_ }}</td>
-                                        <td>{{ $item->type_ }}</td>
-                                        <td>{{ $item->keterangan }}</td>
-                                        <td>Rp. {{ number_format($item->harga_beli, 0, ',', '.') }}</td>
-                                        <td>Rp. {{ number_format($item->hpp, 0, ',', '.') }}</td>
-                                        <td>Rp. {{ number_format($item->sisa, 0, ',', '.') }}</td>
+        </div>
+        <div class="col-lg-12 grid-margin stretch-card p-0 mt-3">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-sm table-hover">
+                        <thead>
+                            <tr>
+                                <th>NO</th>
+                                <th>MG</th>
+                                <th>PERIODE</th>
+                                <th>TANGGAL</th>
+                                <th>PABRIK</th>
+                                <th>REG</th>
+                                <th>PETANI</th>
+                                <th>NO SPTA</th>
+                                <th>NO TRUK</th>
+                                <th>BOBOT</th>
+                                <th>VAR</th>
+                                <th>TYPE</th>
+                                <th>KET</th>
+                                <th>BELI</th>
+                                <th>HPP</th>
+                                <th>SISA</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1; ?>
+                            <?php $sisa = 0; ?>
+                            <?php $bobot = 0; ?>
+                            @foreach ($data as $item)
+                            <?php $sisa += $item->sisa; ?>
+                            <?php $bobot += $item->bobot; ?>
+                            <tr style="height: 52px;">
+                                <td>{{ $no++ }}</td>
+                                <td>{{ $item->masa_giling }}</td>
+                                <td>{{ $item->periode }}</td>
+                                <td>{{ date('d/M/Y', strtotime($item->created_at)) }}</td>
+                                <td>{{ $item->pabrik }}</td>
+                                <td>{{ $item->reg }}</td>
+                                <td>{{ $item->petani }}</td>
+                                <td>{{ $item->nospta }}</td>
+                                <td>{{ $item->nopol }}</td>
+                                <td>{{ $item->bobot }}</td>
+                                <td>{{ $item->variasi_ }}</td>
+                                <td>{{ $item->type_ }}</td>
+                                <td>{{ $item->keterangan }}</td>
+                                <td>Rp. {{ number_format($item->harga_beli, 0, ',', '.') }}</td>
+                                <td>Rp. {{ number_format($item->hpp, 0, ',', '.') }}</td>
+                                <td>Rp. {{ number_format($item->sisa, 0, ',', '.') }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="row mt-5">
+                        <div class="col-md-12 pt-2">
+                            <table class="table table-dark w-100">
+                                <tbody>
+                                    <tr class="bg-dark">
+                                        <td></td>
+                                        <td>Total Bobot</td>
+                                        <td>:</td>
+                                        <td>{{ $bobot }} KW</td>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        <div class="row mt-5">
-                            <div class="col-md-12 pt-2">
-                                <table class="table table-dark w-100">
-                                    <tbody>
-                                        <tr class="bg-dark">
-                                            <td></td>
-                                            <td>Total Bobot</td>
-                                            <td>:</td>
-                                            <td>{{ $bobot }} KW</td>
-                                        </tr>
-                                        <tr>
-                                            <td></td>
-                                            <td>Total Sisa</td>
-                                            <td>:</td>
-                                            <td>Rp. {{ number_format($sisa, 0, ',', '.') }}</td>
-                                        </tr>
-                                    </tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td>Total Sisa</td>
+                                        <td>:</td>
+                                        <td>Rp. {{ number_format($sisa, 0, ',', '.') }}</td>
+                                    </tr>
+                                </tbody>
 
-                                </table>
-                            </div>
+                            </table>
                         </div>
-                        {{-- <div class="row mt-5" style="border-top: 1px #515151 solid !important;">
+                    </div>
+                    {{-- <div class="row mt-5" style="border-top: 1px #515151 solid !important;">
                             <div class="col-md-6"></div>
                             <div class="col-md-6 pt-2">
                                 <table class="w-100">
@@ -103,96 +103,96 @@
                                         </tr>
                                         <tr class="col-sm-2 d-flex flex-column px-0">
                                             <td>{{ $bobot }} KW</td>
-                                            <td>Rp. {{ number_format($sisa, 0, ',', '.') }}</td>
-                                        </tr>
-                                    </tbody>
+                    <td>Rp. {{ number_format($sisa, 0, ',', '.') }}</td>
+                    </tr>
+                    </tbody>
 
-                                </table>
+                    </table>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+</div>
+</div>
+<footer class=" footer">
+    <div class="container-fluid clearfix">
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
+    </div>
+</footer>
+</div>
+<!-- modal untuk tambah data -->
+<div class="modal fade" id="modal-md-filter">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <form action="{{ url('/laporan') }}" method="post">
+                @csrf
+                <div class="modal-header">
+                    <h3 class="modal-title">Filter</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Tanggal Awal</label>
+                                <input type="date" class="form-control text-dark" name="tanggalaw" id="taw">
+                                <span class="text-dark"></span>
                             </div>
-                        </div> --}}
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Tanggal Akhir</label>
+                                <input type="date" class="form-control text-dark" name="tanggalak" id="tak">
+                                <span class="text-dark"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="pabrik">Pabrik</label>
+                        <select class="form-control text-dark" name="pabrik" id="pabrik">
+                            <option selected value="">Pilih</option>
+                            @foreach ($pabrik as $item)
+                            <option value="{{ $item->id_pabrik }}">{{ $item->nama_pabrik }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="masa">Masa Giling</label>
+                        <input type="text" name="masa" value="{{ date('Y') }}" id="masa" class="form-control texk-dark">
+                    </div>
+                    <div class="form-group">
+                        <label for="periode">Periode</label>
+                        <select class="form-control text-dark" name="periode" id="periode" data-change="add">
+                            <option selected value="">Pilih</option>
+                            <option>1</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Type Tebu</label>
+                        <select class="form-control text-dark" name="type" id="type">
+                            <option selected value="">Pilih</option>
+                            @foreach ($type as $item)
+                            <option value="{{ $item->type }}">{{ $item->type }}</option>
+                            @endforeach
+                        </select>
                     </div>
                 </div>
-            </div>
+                <!-- /.card-body -->
+                <div class="card-footer">
+                    <button type="submit" id="filter" class="btn btn-gradient-success">Cari</button>
+                </div>
+            </form>
         </div>
-        <footer class=" footer">
-            <div class="container-fluid clearfix">
-                <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates </a> from Bootstrapdash.com</span>
-            </div>
-        </footer>
     </div>
-    <!-- modal untuk tambah data -->
-    <div class="modal fade" id="modal-md-filter">
-        <div class="modal-dialog modal-md">
-            <div class="modal-content">
-                <form action="{{ url('/laporan') }}" method="post">
-                    @csrf
-                    <div class="modal-header">
-                        <h3 class="modal-title">Filter</h3>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Tanggal Awal</label>
-                                    <input type="date" class="form-control text-dark" name="tanggalaw" id="taw">
-                                    <span class="text-dark"></span>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Tanggal Akhir</label>
-                                    <input type="date" class="form-control text-dark" name="tanggalak" id="tak">
-                                    <span class="text-dark"></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="pabrik">Pabrik</label>
-                            <select class="form-control text-dark" name="pabrik" id="pabrik">
-                                <option selected value="">Pilih</option>
-                                @foreach ($pabrik as $item)
-                                    <option value="{{ $item->id_pabrik }}">{{ $item->nama_pabrik }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="masa">Pabrik</label>
-                            <input type="text" name="masa" value="{{ date('Y') }}" id="masa" class="form-control texk-dark">
-                        </div>
-                        <div class="form-group">
-                            <label for="periode">Periode</label>
-                            <select class="form-control text-dark" name="periode" id="periode" data-change="add">
-                                <option selected value="">Pilih</option>
-                                <option>1</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Type Tebu</label>
-                            <select class="form-control text-dark" name="type" id="type">
-                                <option selected value="">Pilih</option>
-                                @foreach ($type as $item)
-                                    <option value="{{ $item->type }}">{{ $item->type }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
-                    <div class="card-footer">
-                        <button type="submit" id="filter" class="btn btn-gradient-success">Cari</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <!-- /.modal-content -->
-    </div>
-    <!-- /.modal-dialog -->
-    </div>
+    <!-- /.modal-content -->
+</div>
+<!-- /.modal-dialog -->
+</div>
 @endsection
 @section('specific-js')
-    <script src="{{ asset('assets/js/function/Laporan.js') }}"></script>
+<script src="{{ asset('assets/js/function/Laporan.js') }}"></script>
 @endsection
