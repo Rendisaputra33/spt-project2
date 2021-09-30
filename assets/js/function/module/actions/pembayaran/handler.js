0c2401565ebff48dd94d0e79b0396d2bbb4ad165
Rendisaputra33 <gorengmbah@gmail.com>
2021-09-30 16:09:56 +0700

    update

--- assets/js/function/module/actions/pembayaran/handler.js
+++ assets/js/function/module/actions/pembayaran/handler.js
@@ -1,30 +1,30 @@
-import elements, { elementList } from '../../elements/pembayaran/index.js';
-import { getDetail, getFilter } from './request.js';
-import elglobal from '../../elements/index.js';
-import { setListDetail, setListFilter, swalDelete } from './setter.js';
+import elements, { elementList } from "../../elements/pembayaran/index.js";
+import { getDetail, getFilter } from "./request.js";
+import elglobal from "../../elements/index.js";
+import { setListDetail, setListFilter, swalDelete } from "./setter.js";
 
 export function handlerDelete(e) {
-    // delete the event
-    e.preventDefault();
-    // if confirm delete this data
-    swalDelete(e.target.href);
+	// delete the event
+	e.preventDefault();
+	// if confirm delete this data
+	swalDelete(this.href);
 }
 
 export async function handlerDetail(e) {
-    // get data form server
-    const data = await getDetail(this.getAttribute('data-id'));
-    // set data to view
-    setListDetail(data.data);
+	// get data form server
+	const data = await getDetail(this.getAttribute("data-id"));
+	// set data to view
+	setListDetail(data.data);
 }
 
 export async function handlerFilter(e) {
-    // instance variable
-    const selected = elements?.inputFilter?.value;
-    const data = await getFilter(selected);
-    // set data to view
-    setListFilter(data.data);
+	// instance variable
+	const selected = elements?.inputFilter?.value;
+	const data = await getFilter(selected);
+	// set data to view
+	setListFilter(data.data);
 }
 
 export function handlerUpdateHarga(e) {
-    return elements.formUpdate?.setAttribute('action', elglobal.baseUrl + '/pembayaran/' + this.getAttribute('data-id'));
+	return elements.formUpdate?.setAttribute("action", elglobal.baseUrl + "/pembayaran/" + this.getAttribute("data-id"));
 }
