565458f49a0d1022246824fad696bb28bd722898
Rendisaputra33 <gorengmbah@gmail.com>
2021-10-25 08:43:46 +0700

    update

--- assets/js/function/module/actions/entry/handler.js
+++ assets/js/function/module/actions/entry/handler.js
@@ -1,20 +1,20 @@
-import { requestUpdate } from './request.js';
-import { setFormUpdate } from './setter.js';
+import { requestUpdate } from "./request.js";
+import { setFormUpdate } from "./setter.js";
 
-export const handlerUpdate = async (This) => {
-    try {
-        const result = await requestUpdate(This.getAttribute('data-id'));
-        setFormUpdate(result);
-    } catch (error) {
-        console.log(error);
-    }
+export const handlerUpdate = async object => {
+	try {
+		const result = await requestUpdate(object.getAttribute("data-id"));
+		setFormUpdate(result);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-export const handlerDetail = async (This) => {
-    try {
-        const result = await requestUpdate(This.getAttribute('data-id'));
-        return result;
-    } catch (error) {
-        console.log(error);
-    }
+export const handlerDetail = async object => {
+	try {
+		const result = await requestUpdate(object.getAttribute("data-id"));
+		return result;
+	} catch (error) {
+		console.log(error);
+	}
 };
