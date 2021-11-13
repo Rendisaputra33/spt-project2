84fb38f643a37970dc10bea61964bea713ebe33d
Rendisaputra33 <gorengmbah@gmail.com>
2021-11-13 20:55:58 +0700

    update

--- project/resources/views/cetak-harga-beli.blade.php
+++ project/resources/views/cetak-harga-beli.blade.php
@@ -77,52 +77,44 @@ function formatTanggal($tgl)
                 <div class="col-md-12 mt-4">
                     <table class="cetak table table-sm table-borderless border border-5 border-dark w-100" style="table-layout: fixed;">
                         <tr class=" text-bold text-center border border-bottom-2 border-dark w-100" id="header">
-                            <th class="small py-2 px-0">NO</th>
-                            <th class="small py-2 px-0">MG</th>
-                            <th class="medium py-2 px-0">PERIODE</th>
-                            <th class="py-2 px-0">TGL</th>
-                            <th class="medium py-2 px-0">PABRIK</th>
-                            <th class="medium py-2 px-0">REG</th>
-                            <th class="py-2 px-0">PETANI</th>
-                            <th class="medium py-2 px-0">NO SPTA</th>
-                            <th class="medium py-2 px-0">NO TRUK</th>
-                            <th class="medium py-2 px-0">BOBOT</th>
-                            <th class="small py-2 px-0">VAR</th>
-                            <th class="small py-2 px-0">TYPE</th>
-                            <th class="medium py-2 px-0">PENGIRIM</th>
-                            <th class="py-2 px-0">BELI</th>
-                            <th class="py-2 px-0">HPP</th>
-                            <th class="py-2 px-0">SISA</th>
+                            <th>NO</th>
+                            <th>MG</th>
+                            <th>PERIODE</th>
+                            <th>TGL</th>
+                            <th>PABRIK</th>
+                            <th>NO SPTA</th>
+                            <th>NO TRUK</th>
+                            <th>BOBOT</th>
+                            <th>VAR</th>
+                            <th>TYPE</th>
+                            <th>BELI</th>
+                            <th>TOTAL</th>
                         </tr>
                         <tbody>
                             <?php $sisa = 0; ?>
                             <?php $bobot = 0; ?>
                             @foreach ($data as $item)
-                                <?php $item->harga_beli && $item->hpp ? ($sisa += ($item->hpp - $item->harga_beli) * $item->bobot) : ($sisa += 0); ?>
+                                <?php $sisa += $item->harga_beli * $item->bobot; ?>
                                 <?php $bobot += $item->bobot; ?>
                                 <tr class="text-capitalize text-center w-auto">
-                                    <td class="small px-0">{{ $loop->iteration }}</td>
-                                    <td class="small px-0">{{ $item->masa_giling }}</td>
-                                    <td class="medium px-0">{{ $item->periode }}</td>
-                                    <td class="px-0">{{ date('d/m/Y', strtotime($item->created_at)) }}</td>
-                                    <td class="medium px-0">{{ $item->pabrik }}</td>
-                                    <td class="medium px-0">{{ $item->reg }}</td>
-                                    <td class="px-0">{{ $item->petani }}</td>
-                                    <td class="medium px-0">{{ $item->nospta }}</td>
-                                    <td class="medium px-0">{{ $item->nopol }}</td>
-                                    <td class="medium px-0">{{ $item->bobot }}</td>
-                                    <td class="small px-0">{{ $item->variasi_ }}</td>
-                                    <td class="small px-0">{{ $item->type_ }}</td>
-                                    <td class="medium px-0">{{ $item->nama_pengirim ? $item->nama_pengirim : '-' }}</td>
-                                    <td class="px-0">{{ $item->harga_beli ? 'Rp. ' . number_format($item->harga_beli, 0, ',', '.') : '-' }}
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->masa_giling }}</td>
+                                    <td>{{ $item->periode }}</td>
+                                    <td class="">{{ date('d/m/Y', strtotime($item->created_at)) }}</td>
+                                    <td>{{ $item->pabrik }}</td>
+                                    <td>{{ $item->nospta }}</td>
+                                    <td>{{ $item->nopol }}</td>
+                                    <td>{{ $item->bobot }}</td>
+                                    <td>{{ $item->variasi_ }}</td>
+                                    <td>{{ $item->type_ }}</td>
+                                    <td>{{ $item->harga_beli ? 'Rp. ' . number_format($item->harga_beli, 0, ',', '.') : '-' }}
                                     </td>
-                                    <td class="px-0">Rp. {{ number_format($item->hpp, 0, ',', '.') }}</td>
-                                    <td class="px-0">{{ $item->harga_beli && $item->hpp ? 'Rp. ' . number_format(($item->hpp - $item->harga_beli) * $item->bobot, 0, ',', '.') : '-' }}</td>
+                                    <td>{{ 'Rp. ' . number_format($item->harga_beli * $item->bobot, 0, ',', '.') }}</td>
                                 </tr>
                             @endforeach
 
                             <tr class="border border-top-2 border-dark">
-                                <td class="py-2" colspan="11" class=""></td>
+                                <td class="py-2" colspan="7" class=""></td>
                                 <td class=" py-2" colspan="2" class="">
                                     Total Bobot : {{ $bobot }} Kwintal
                                 </td>
